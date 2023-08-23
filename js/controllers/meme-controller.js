6ae715ecf05552410490ab7896d74027999e33c1
Raz <amsalemraz10@gmail.com>
2023-08-23 12:03:44 +0300

    You can make a basic Meme

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -11,11 +11,25 @@ function onInit() {
     renderMeme()
 }
 function renderMeme() {
-    let meme = getMeme()
+    const meme = getMeme()
+    const memeTxt = meme.lines[0].txt
 
     const elImg = new Image()
     elImg.src = `img/${meme.selectedImgId}.jpg`
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
+    
+        gCtx.fillStyle = 'white'
+        gCtx.font = '30px Arial'
+        gCtx.textAlign = 'center'
+        gCtx.fillText(memeTxt, gElCanvas.width / 2, 40)
     }
+}
+
+function updateMemeText() {
+    const newText = document.querySelector('.text-line').value
+    const meme = getMeme()
+    meme.lines[0].txt = newText
+    // console.log(gMeme.lines[0].txt)
+    renderMeme()
 }
